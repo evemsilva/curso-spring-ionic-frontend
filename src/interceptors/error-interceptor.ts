86d8f0e95a69bb3b965f5a629d9c6015dc02928e
Everton Monte da Silva <everton0189@gmail.com>
2018-04-24 23:03:33 -0300

    Tratando erros 403

--- src/interceptors/error-interceptor.ts
+++ src/interceptors/error-interceptor.ts
@@ -1,9 +1,13 @@
 import { HttpInterceptor, HttpRequest, HttpHandler, HttpEvent, HTTP_INTERCEPTORS } from "@angular/common/http";
 import { Observable } from "rxjs/Rx";
 import { Injectable } from "@angular/core";
+import { StorageService } from "../services/storage.service";
 
 @Injectable()
 export class ErrorInterceptor implements HttpInterceptor {
+
+    constructor(public storage: StorageService){
+    }
 
     intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
 
@@ -25,8 +29,17 @@ export class ErrorInterceptor implements HttpInterceptor {
                 console.log("Erro detectado pelo interceptor");
                 console.log(errorObj);
 
+                switch(errorObj.status){
+                    case 403:
+                        this.handle403();
+                }
+
                 return Observable.throw(errorObj);
             }) as any;
+    }
+
+    handle403(){ // HTTP 403 = Nao Autorizado 
+        this.storage.setLocalUser(null);
     }
 }
 
--- src/services/domain/cliente.service.ts
+++ src/services/domain/cliente.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from "@angular/core";
 import { ClienteDTO } from "../../models/cliente.dto";
 import { Observable } from "rxjs/Rx";
-import { HttpClient, HttpHeaders } from "@angular/common/http";
+import { HttpClient } from "@angular/common/http";
 import { API_CONFIG } from "../../config/api.config";
 
 @Injectable()
