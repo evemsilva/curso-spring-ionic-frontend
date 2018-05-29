5a8e29b7b4009ac779b19b5939c44d7da6be24ea
Everton Silva <everton0189@gmail.com>
2018-05-29 20:11:23 -0300

    Obtendo imagem da galeria

--- src/config/api.config.ts
+++ src/config/api.config.ts
@@ -1,4 +1,4 @@
 export const API_CONFIG = {
-    baseUrl: 'http://localhost:9090',
+    baseUrl: 'https://curso-spring-ionic-evemsilva.herokuapp.com',
     bucketBaseUrl: 'https://s3-sa-east-1.amazonaws.com/curso-spring-ionic-evemsilva'
 }
